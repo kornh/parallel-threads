aae588a4a6141ebc508ba182c4359cf551acfa91
kornh <kh@h-four.de>
2017-04-06 17:32:48 +0200

    Changed: Method type

--- src/Talk.java
+++ src/Talk.java
@@ -2,31 +2,36 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 
 public class Talk {
 	public static void main(String[] args) throws SocketException, UnknownHostException {
-		
-	    Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
-	    for (; n.hasMoreElements();)
-	    {
-	        NetworkInterface e = n.nextElement();
 
-	        Enumeration<InetAddress> a = e.getInetAddresses();
-	        for (; a.hasMoreElements();)
-	        {
-	            InetAddress addr = a.nextElement();
-	            String addrs = addr.getHostAddress();
-	            try {
-					
-	            	int pre = Integer.parseInt(addrs.substring(0,3));
-	            	if(pre != 127)
-	            		System.out.println(addrs);
+		System.out.println(getAddresses().toString());
+		
+	}
+
+	private static ArrayList<String> getAddresses() throws SocketException {
+		ArrayList<String> liste = new ArrayList<String>();
+		Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+		for (; n.hasMoreElements();) {
+			NetworkInterface e = n.nextElement();
+
+			Enumeration<InetAddress> a = e.getInetAddresses();
+			for (; a.hasMoreElements();) {
+				InetAddress addr = a.nextElement();
+				String addrs = addr.getHostAddress();
+				try {
+
+					int pre = Integer.parseInt(addrs.substring(0, 3));
+					if (pre != 127)
+						liste.add(addrs);
 				} catch (NumberFormatException e2) {
 					// TODO: handle exception
 				}
-	        }
-	    }
-		
+			}
+		}
+		return liste;
 	}
 }
